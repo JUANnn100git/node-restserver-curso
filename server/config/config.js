ae5d3338470c88c697016196b3b7a3bffd1c0fc7
JUANnn100 <jvavillanuevaaguinaga@gmail.com>
2019-03-06 18:25:02 -0500

    Brypt - Encriptando contraseñas  implementado

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,8 @@
 //     Puerto
 // ===============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ===============================
+//     Base de Datos
+// ===============================
+process.env.URLDB = 'mongodb://localhost:27017/cafe';
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 require('./config/config');
-const express = require('express')
-const app = express()
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
 
 const bodyParser = require('body-parser')
 
@@ -11,42 +15,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/usuario', function(req, res) {
+app.use(require('./routes/usuario'));
 
-    res.json('get Usuario');
-});
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
 
-app.post('/usuario', function(req, res) {
+    if (err) throw err;
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
+    console.log('Base de Datos ONLINE');
 
 });
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto ', process.env.PORT);
